186734a071e814c8d38c5f97e85e80433277c99e
Alexandr Angan <devnull@asperus.com>
2016-05-03 15:23:58 +0200

    First Commit

---
+++ TimeSplitter/FormTimeSplitter.Designer.cs
@@ -0,0 +1,182 @@
+﻿namespace TimeSplitter
+{
+    partial class TimeSplitter
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle1 = new System.Windows.Forms.DataGridViewCellStyle();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(TimeSplitter));
+            this.label_time = new System.Windows.Forms.Label();
+            this.textNotes = new System.Windows.Forms.TextBox();
+            this.btn_start = new System.Windows.Forms.Button();
+            this.grid = new System.Windows.Forms.DataGridView();
+            this.Clmn_start = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.clmn_notes = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.clmn_timer = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.timer = new System.Windows.Forms.Timer(this.components);
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            ((System.ComponentModel.ISupportInitialize)(this.grid)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label_time
+            // 
+            this.label_time.Font = new System.Drawing.Font("Tahoma", 20.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label_time.Location = new System.Drawing.Point(2, 13);
+            this.label_time.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label_time.Name = "label_time";
+            this.label_time.Size = new System.Drawing.Size(186, 51);
+            this.label_time.TabIndex = 0;
+            this.label_time.Text = "0:00:00";
+            this.label_time.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // textNotes
+            // 
+            this.textNotes.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.textNotes.Font = new System.Drawing.Font("Tahoma", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.textNotes.Location = new System.Drawing.Point(196, 13);
+            this.textNotes.Margin = new System.Windows.Forms.Padding(4);
+            this.textNotes.Multiline = true;
+            this.textNotes.Name = "textNotes";
+            this.textNotes.Size = new System.Drawing.Size(250, 51);
+            this.textNotes.TabIndex = 1;
+            this.textNotes.KeyUp += new System.Windows.Forms.KeyEventHandler(this.textNotes_KeyUp);
+            // 
+            // btn_start
+            // 
+            this.btn_start.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.btn_start.Location = new System.Drawing.Point(454, 13);
+            this.btn_start.Margin = new System.Windows.Forms.Padding(4);
+            this.btn_start.Name = "btn_start";
+            this.btn_start.Size = new System.Drawing.Size(117, 51);
+            this.btn_start.TabIndex = 2;
+            this.btn_start.Text = "Start";
+            this.btn_start.UseVisualStyleBackColor = true;
+            this.btn_start.Click += new System.EventHandler(this.btn_start_Click);
+            // 
+            // grid
+            // 
+            this.grid.AllowUserToAddRows = false;
+            this.grid.AllowUserToDeleteRows = false;
+            this.grid.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.grid.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.Fill;
+            dataGridViewCellStyle1.Alignment = System.Windows.Forms.DataGridViewContentAlignment.MiddleCenter;
+            dataGridViewCellStyle1.BackColor = System.Drawing.SystemColors.Control;
+            dataGridViewCellStyle1.Font = new System.Drawing.Font("Tahoma", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            dataGridViewCellStyle1.ForeColor = System.Drawing.SystemColors.WindowText;
+            dataGridViewCellStyle1.SelectionBackColor = System.Drawing.SystemColors.Highlight;
+            dataGridViewCellStyle1.SelectionForeColor = System.Drawing.SystemColors.HighlightText;
+            dataGridViewCellStyle1.WrapMode = System.Windows.Forms.DataGridViewTriState.True;
+            this.grid.ColumnHeadersDefaultCellStyle = dataGridViewCellStyle1;
+            this.grid.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.grid.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.Clmn_start,
+            this.clmn_notes,
+            this.clmn_timer});
+            this.grid.Location = new System.Drawing.Point(12, 82);
+            this.grid.Name = "grid";
+            this.grid.ReadOnly = true;
+            this.grid.Size = new System.Drawing.Size(570, 321);
+            this.grid.TabIndex = 3;
+            this.grid.CellMouseClick += new System.Windows.Forms.DataGridViewCellMouseEventHandler(this.grid_CellMouseClick);
+            // 
+            // Clmn_start
+            // 
+            this.Clmn_start.AutoSizeMode = System.Windows.Forms.DataGridViewAutoSizeColumnMode.None;
+            this.Clmn_start.HeaderText = "Start";
+            this.Clmn_start.Name = "Clmn_start";
+            this.Clmn_start.ReadOnly = true;
+            this.Clmn_start.Width = 150;
+            // 
+            // clmn_notes
+            // 
+            this.clmn_notes.AutoSizeMode = System.Windows.Forms.DataGridViewAutoSizeColumnMode.Fill;
+            this.clmn_notes.HeaderText = "Notes";
+            this.clmn_notes.Name = "clmn_notes";
+            this.clmn_notes.ReadOnly = true;
+            // 
+            // clmn_timer
+            // 
+            this.clmn_timer.AutoSizeMode = System.Windows.Forms.DataGridViewAutoSizeColumnMode.None;
+            this.clmn_timer.HeaderText = "Timer";
+            this.clmn_timer.Name = "clmn_timer";
+            this.clmn_timer.ReadOnly = true;
+            // 
+            // timer
+            // 
+            this.timer.Enabled = true;
+            this.timer.Tick += new System.EventHandler(this.timer_Tick);
+            // 
+            // timer1
+            // 
+            this.timer1.Enabled = true;
+            this.timer1.Interval = 600000;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // TimeSplitter
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 19F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(594, 410);
+            this.Controls.Add(this.grid);
+            this.Controls.Add(this.btn_start);
+            this.Controls.Add(this.textNotes);
+            this.Controls.Add(this.label_time);
+            this.Font = new System.Drawing.Font("Tahoma", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Margin = new System.Windows.Forms.Padding(4);
+            this.MaximizeBox = false;
+            this.MinimumSize = new System.Drawing.Size(600, 250);
+            this.Name = "TimeSplitter";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Time splitter";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.TimeSplitter_FormClosing);
+            ((System.ComponentModel.ISupportInitialize)(this.grid)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label_time;
+        private System.Windows.Forms.TextBox textNotes;
+        private System.Windows.Forms.Button btn_start;
+        private System.Windows.Forms.DataGridView grid;
+        private System.Windows.Forms.Timer timer;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Clmn_start;
+        private System.Windows.Forms.DataGridViewTextBoxColumn clmn_notes;
+        private System.Windows.Forms.DataGridViewTextBoxColumn clmn_timer;
+        private System.Windows.Forms.Timer timer1;
+    }
+}
+
---
+++ TimeSplitter/FormTimeSplitter.cs
@@ -0,0 +1,263 @@
+﻿using System;
+using System.Text;
+using System.Windows.Forms;
+using System.IO;
+using System.Threading;
+using System.Windows;
+using System.Runtime.InteropServices;
+
+namespace TimeSplitter
+{
+    public partial class TimeSplitter : Form
+    {
+        bool paused = true;
+        DateTime time_start;
+        bool idleTime = false;
+        bool msgBoxConfirmAnswer = true;
+        string notesFileName;
+        TimeSpan idleTimeCorrection = new TimeSpan(0, 10, 0);
+
+        public TimeSplitter()
+        {
+            InitializeComponent();
+
+            string today = DateTime.Today.ToShortDateString();
+            notesFileName = "TimeSplitter_" + today + ".txt";
+
+            load_records_from_file();
+
+            textNotes.Text = "Idle Time";
+            start_timer();
+        }
+
+        private void msgBoxExt_msgBoxResultEvent(object sender, MsgBoxResultEventArgs e)
+        {
+            if (e.resultButton == DialogResult.None)
+            {
+                msgBoxConfirmAnswer = false;
+            }
+
+            if (e.resultButton == DialogResult.None || e.resultButton == DialogResult.No)
+            {
+                idleTime = true;
+            }
+        }
+
+        private void btn_start_Click(object sender, EventArgs e)
+        {
+            if (paused)
+            {
+                timer1.Enabled = false;
+                timer1.Enabled = true;
+                start_timer();
+            }
+            else
+            {
+                stop_timer();
+            }
+        }
+
+        private void start_timer()
+        {
+            paused = false;
+            btn_start.Text = "Stop";
+            textNotes.Enabled = false;
+            time_start = DateTime.Now;
+            label_time.Text = "00:00:00";
+
+            int rowCount = grid.Rows.Count;
+
+            try
+            {
+                for (int curRow = 0; curRow < rowCount; curRow++)
+                {
+                    String clmn_notes = (String)grid["clmn_notes", curRow].Value;
+
+                    if (textNotes.Text == clmn_notes)
+                    {
+                        string stringTime = (String)grid["clmn_timer", curRow].Value;
+                        string[] timerValues = stringTime.Split(':');
+
+                        TimeSpan timerSpan = new TimeSpan(Int32.Parse(timerValues[0]), Int32.Parse(timerValues[1]), Int32.Parse(timerValues[2]));
+
+                        time_start = time_start - timerSpan;
+
+                        if (msgBoxConfirmAnswer == false)
+                        {
+                            time_start = time_start - idleTimeCorrection;
+                            msgBoxConfirmAnswer = true;
+                        }
+
+                        label_time.Text = time_start.ToString("HH:mm:ss");
+                    }
+                }
+            }
+            catch (System.Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+
+        private void stop_timer()
+        {
+            paused = true;
+            //timer.Enabled = false;
+            add_record_to_grid();
+            //save_records_to_file();
+
+            if (textNotes.Text != "Idle Time")
+            {
+                textNotes.Text = "Idle Time";
+                start_timer();
+                return;
+            }
+
+            textNotes.Enabled = true;
+            label_time.Text = "00:00:00";
+            btn_start.Text = "Start";
+            textNotes.Focus();
+        }
+
+        private void save_records_to_file()
+        {
+            try
+            {
+                File.Delete(notesFileName);
+
+                for (int curRow = 0; curRow < grid.Rows.Count; curRow++)
+                {
+                    File.AppendAllText(notesFileName, grid["Clmn_start", curRow].Value + "\t"
+                                    + grid["clmn_notes", curRow].Value + "\t"
+                                    + grid["clmn_timer", curRow].Value
+                                    + Environment.NewLine, Encoding.UTF8);
+                }
+            }
+            catch (System.Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+
+        private void add_record_to_grid()
+        {
+            bool recordNameAlreadyPresent = false;
+
+            for (int curRow = 0; curRow < grid.Rows.Count; curRow++)
+            {
+                if ((string)(grid["clmn_notes", curRow].Value) == textNotes.Text)
+                {
+                    grid["Clmn_start", curRow].Value = time_start.ToString("yyyy-MM-dd HH:mm");                    
+                    grid["clmn_notes", curRow].Value = textNotes.Text.TrimEnd();
+                    grid["clmn_timer", curRow].Value = label_time.Text;
+
+                    recordNameAlreadyPresent = true;
+                }
+            }
+
+            if (!recordNameAlreadyPresent)
+            {
+                int row = grid.Rows.Count;
+                grid.Rows.Add();
+                grid["Clmn_start", row].Value = time_start.ToString("yyyy-MM-dd HH:mm");
+                grid["clmn_notes", row].Value = textNotes.Text.TrimEnd();
+                grid["clmn_timer", row].Value = label_time.Text;
+            }
+        }
+
+        private void TimeSplitter_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            if (!paused)
+            {
+                stop_timer();
+            }
+
+            save_records_to_file();
+        }
+
+        private void timer_Tick(object sender, EventArgs e)
+        {
+            if (paused)
+            {
+                return;
+            }
+
+            if (idleTime && textNotes.Text != "Idle Time")
+            {
+                idleTime = false;
+                stop_timer();
+            }
+
+            TimeSpan time_duration = (DateTime.Now - time_start).Duration();
+            string time_str = time_duration.ToString();
+
+            string[] Split = time_str.ToString().Split('.');
+            label_time.Text = Split[0];
+        }
+
+        private void grid_CellMouseClick(object sender, DataGridViewCellMouseEventArgs e)
+        {
+            if (e.RowIndex == -1 || e.ColumnIndex != 1)
+            {
+                return;
+            }
+
+            add_record_to_grid();
+            //save_records_to_file();
+            textNotes.Text = grid.SelectedCells[0].Value.ToString();
+            start_timer();
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            msgBoxExt.msgBoxResultEvent += new EventHandler<MsgBoxResultEventArgs>(msgBoxExt_msgBoxResultEvent);
+
+            //timeout in ms
+            int timeout = 60000; //1 min
+
+            // is model
+            bool model = false;
+
+            //new MsgBoxExtOptions
+            MsgBoxExtOptions options = new MsgBoxExtOptions
+               ("\nCurrent pastime:\t" + textNotes.Text + "\n\n" + " Do you want to continue ?",
+               "Time Splitter: Pastime confirmation", MsgBoxResultReferences.EMPTY,
+                MessageBoxButtons.YesNo, MessageBoxIcon.Question, timeout, model);
+
+            //show messagebox
+            msgBoxExt.Show(options);
+        }
+
+        private void textNotes_KeyUp(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.Enter)
+            {
+                btn_start_Click(null, null);
+            }
+        }
+
+        private void load_records_from_file()
+        {
+            try
+            {
+                if (!File.Exists(notesFileName))
+                {
+                    MessageBox.Show("File " + notesFileName + " doesn't exist", "Error");
+                    return;
+                }
+
+                string[] lines = File.ReadAllLines(notesFileName);
+
+                foreach (string curLine in lines)
+                {
+                    string[] cellContent = curLine.Split('\t');
+                    grid.Rows.Add(cellContent[0], cellContent[1], cellContent[2]);
+                }                
+            }
+            catch (System.Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+
+    }
+}
